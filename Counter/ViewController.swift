2d86aba787797ce7e5169bf912e1becc9fe6a6e8
kolokolnikovmb <kolokolnikovmb@yandex.ru>
2022-11-08 12:28:42 +0300

    Start of the project

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -8,12 +8,19 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var counterTimes: Int = 0
+    @IBOutlet weak var labelCounter: UILabel!
+    @IBOutlet weak var buttonCounetr: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func buttonDidTap(_ sender: Any) {
+        counterTimes += 1
+        labelCounter.text = "Значение счётчика: \(counterTimes)"
+    }
+    
 }
 
